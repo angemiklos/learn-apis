98506e0f54633669f55dc55a7277c4df9e530077
Ange Miklos <angejuba@gmail.com>
2018-03-13 14:36:09 -0700

    fixing some apis..still needs work

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const WURL = `https://www.metaweather.com/api//api/location/search/?query=`;
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const MURL = `https://developers.themoviedb.org/3/configuration/get-api-configuration`;
 const XKCD_PREFIX = `https://xkcd.com/`;
-const XKCD_SUFFIX = `info.0.json`;
+const XKCD_SUFFIX = `/info.0.json`;
 const DOG_URL_PREFIX = `https://dog.ceo/api/breed/`;
 const DOG_URL_SUFFIX = `/images/random`;
 const CAT_URL_PREFIX = `https://http.cat/`;
@@ -14,7 +14,7 @@ const STOCKS_URL_PREFIX = `https://api.iextrading.com/1.0/stock/market/batch?sym
 const STOCKS_URL_SUFFIX = `&types=quote&range=1m&last=5`;
 
 function displayResults1(res) {
-	console.log(res.num);
+    console.log(res.num);
    $(".xkcd-api").html(`
    	<div>
    	    <p>XKCD Cartoon ${res.num}: ${res.title}</p>
@@ -29,19 +29,21 @@ function getApi1(cartoonNumber) {
 	if (cartoonNumber == 0) {
 		url = XKCD_PREFIX + XKCD_SUFFIX;
 	} else {
-		url = XKCD_PREFIX + cartoonNumber + '/' + XKCD_SUFFIX;
+		url = XKCD_PREFIX + cartoonNumber + XKCD_SUFFIX;
 	}
-
+	console.log("the url is: " + url);
+    displayResults1("Can't get this to work no matter what.");
 	$.ajax({
 		   type : "GET",		   
-		   url : url,
-		   success : displayResults1,
-	});
+		   dataType : "jsonp",
+		   jsonpCallback : 'displayResults1',
+		   url : url});
+
 }
 
 function displayResults2(res) {
    $(".dog-api").html(`
-   	<img src=${res.message} alt="a dog photo"></img>
+   	<img src=${res.message} alt="a dog photo" height=100 width=100 ></img>
    	`);
 }
 
@@ -56,45 +58,100 @@ function getApi2(dogBreed) {
 }
 
 function displayResults3(res) {
+	console.log("I got here" + res);
    $(".cat-api").html(`
-   	<img src=${res} alt="a cat photo"></img>
+   	<p>${res}</p>
    	`);
+//   $(".cat-api").html(`
+ //  	<img src=${res} alt="a cat photo"></img>
+ //  	`);
 }
 
 function getApi3(statusCode) {
+	let url = CAT_URL_PREFIX + statusCode;
 	console.log( statusCode );
+	console.log("the url is: " + url);
+	displayResults3("Refused to execute script from url because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled.")
+
+//	$.ajax({
+//		   type : "GET",		   
+//		   url : url,
+//         contentType   : "text/html; charset=utf-8",
+//		   dataType : "jsonp",
+//		   jsonpCallback : 'displayResults3',
+//	});
+}
+
+function displayResults4(res) {
+	   let newObj;
+	   let text1 = "";
+	   let text2 = "";
+	   	for (let key in res) {
+            newObj = res[key];
+            text1 += "<p>res[ " + key + "] = " + newObj + "</p>";
+	   	}
+	   	for (let key in newObj) {
+            text2 += "<p>res[ " + key + "] = " + newObj[key] + "</p>";
+	   	}
+   $(".anim-api").html(`
+   	    <p>This individual is a ${newObj[0].individual_taxon_canonical_name}</p>
+   	`);
+ //  	<img src=${res} alt="a tracked animal photo"></img>
+ //  	`);
+}
+
+function getApi4(study, individual) {
+	let url = ANIMAL_URL_PREFIX + study + 
+		         ANIMAL_INDIVIDUALS + individual + 
+		         ANIMAL_URL_SUFFIX;
+	console.log( "Study is: " + study );
+	console.log( "Individual is: " + individual );
+	console.log("the url is: " + url);
 
 	$.ajax({
 		   type : "GET",		   
-		   url : CAT_URL_PREFIX + statusCode,
-		   success : displayResults3,
+		   url : url,
+		   dataType : "jsonp",
+		   jsonpCallback : 'displayResults4',
 	});
 }
 
 function displayResults5(res) {
-	console.log("I was here");
-   $(".money-api").html(`
-   	<p>base : ${res.base}</p>
-   	<p>date : ${res.date}</p>
-   	<p>CAD : ${res.rates.CAD}</p>
-   	<p>CHF : ${res.rates.CHF}</p>
-   	<p>EUR : ${res.rates.EUR}</p>
-   	<p>GBP : ${res.rates.GBP}</p>
-   	`);
+    console.log("I was here: " + res);
+    let newObj;
+    let noAccess = false;
+    let text1 = "";
+    let text2 = "";
+   	for (let key in res) {
+        newObj = res[key];
+        text1 += "<p>res[ " + key + "] = " + newObj + "</p>";
+   	}
+   	for (let key in newObj) {
+        text2 += "<p>res[ " + key + "] = " + newObj[key] + "</p>";
+        if (key == 'type' && newObj[key] == "missing_access_key") {
+        	noAccess = true;
+        }
+   	}
+	if (noAccess) {
+        text1 = `<p>Missing Access Key</p>`;
+        text2 = "";
+	}
+    $(".money-api").html(text1 + text2);
 }
 
 function getApi5() {
-
+    console.log("needs an API access key.");
 	$.ajax({
 		   type : "GET",		   
 		   url : MONEY_URL,
-		   success : displayResults5,
+           success : displayResults5,
 	});
 }
 
 function displayResults6(res) {
 	let text = "";
 	let stock;
+
 	for (key in res) {
 		stock = res[key];
 		break;
@@ -104,20 +161,22 @@ function displayResults6(res) {
 }
 
 function getApi6(symbol) {
+	let url = STOCKS_URL_PREFIX + symbol + STOCKS_URL_SUFFIX;
 	console.log("The symbol is: " + symbol);
+	console.log("The url is: " + url);
 
 	$.ajax({
 		   type : "GET",		   
-		   url : STOCKS_URL_PREFIX + symbol + STOCKS_URL_SUFFIX,
+		   url : url,
 		   success : displayResults6,
 	});
 }
 
 function main() {
+
 	$(".js-form1").submit(event => {
 		event.preventDefault();
 		let $cartoon = $(".js-num").val();
-		console.log("cartoon number is: " + $cartoon);
 		getApi1($cartoon);
 	});
 	$(".js-form2").submit(event => {
@@ -137,6 +196,12 @@ function main() {
 		let $study = $(".js-study").val();
 		let $individ = $(".js-individ").val();
 		console.log("study and individ are: " + $study + " and " + $individ);
+		if ($study == "") {
+			$study = '2911040';
+		}
+		if ($individ == "") {
+			$individ = '4262-84830876';
+		}
 		getApi4($study,$individ);
 	});
 	$(".js-form5").submit(event => {
